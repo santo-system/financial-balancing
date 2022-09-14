67304d2414a05d60b5b26d456e66af02709fc8e0
Anderson Santo <kalangoti@gmail.com>
2022-09-14 01:32:27 -0300

    feat: gradle config init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,26 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val logbackVersion: String by project
+
 plugins {
+    base
     kotlin("jvm") version "1.7.10"
     application
 }
 
 group = "com.santosystem"
 version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
     testImplementation(kotlin("test"))
 }
 
@@ -21,7 +29,10 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }
 
 application {
